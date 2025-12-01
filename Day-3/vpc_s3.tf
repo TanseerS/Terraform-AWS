cf4a4a7a547d0255ea199e437962b0ab2d7698c7
TanseerS <tanseer@skillminds.in>
2025-12-01 10:22:02 +0530

    Day - 3 updates

---
+++ Day-3/vpc_s3.tf
@@ -0,0 +1,21 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
+resource "random_id" "bucket_suffix" {
+  byte_length = 4
+}
+
+resource "aws_s3_bucket" "assets" {
+  bucket = lower("assets-${random_id.bucket_suffix.hex}")
+  acl    = "private"
+
+  tags = {
+    Name = "assets-bucket"
+    VPC  = aws_vpc.main.id
+  }
+}
